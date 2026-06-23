14c8354b160396049a57f7545e073467d58eeaae
saurabh305 <saurabhchandra305@gmail.com>
2026-06-23 20:42:05 +0530

    Fixed failling tests on github actions

--- ProductCatalog.Api/Program.cs
+++ ProductCatalog.Api/Program.cs
@@ -1,6 +1,11 @@
+using System;
 using System.Collections.Concurrent;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    Args = args,
+    ContentRootPath = AppContext.BaseDirectory
+});
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -17,6 +22,7 @@ var summaries = new[]
 {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
+
 
 // In-memory product store
 var products = new ConcurrentDictionary<Guid, Product>();
